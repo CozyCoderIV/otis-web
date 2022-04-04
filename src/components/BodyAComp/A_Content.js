ce5b45b7b6b93daf2080e044e04e63b1709f7276
Otis Jackson IV <ojacks7@lsu.edu>
2022-04-04 15:36:06 -0500

    init

--- src/components/BodyAComp/A_Content.js
+++ src/components/BodyAComp/A_Content.js
@@ -7,16 +7,48 @@ function A_Content() {
       <div className='aa-container'>
 
         <div className='aa-title-box'> 
-          <h3 className='aa-title'>Who is Otis IV ??</h3>
+          <h2 className='aa-title'><i>Who is Otis IV ??</i></h2>
         </div>
 
+        {/* <div className='icon-box'>
+          <img className='icon' src='media/WebpageLogo_edited.png' alt=''></img>
+        </div> */}
+
         <div className='aa-content-box'>
-          <div className='aa-content-1'></div>
-          <div className='aa-content-2'></div>
+          
+          <div className='aa-content-1'>
+            <ol className='aa-list'>
+              <li>
+                <div className='aa-list-item'>
+                  <h3>Origin Story</h3>
+                </div>
+              </li>
+              <li>
+                <div className='aa-list-item'>
+                  <h3>Educational Background</h3>
+                </div>
+              </li>
+              <li>
+                <div className='aa-list-item'>
+                  <h3>Hobbies / Interests</h3>
+                </div>
+              </li>
+              <li>
+                <div className='aa-list-item'>
+                  <h3>Current Milestones</h3>
+                </div>
+              </li>
+            </ol>
+          </div>
+
+          <div className='aa-content-2'>
+            <div className='aa-img-container'></div>
+          </div>
+
         </div>
 
       </div>
-      
+
     </div>
   )
 }
--- src/components/BodyBComp/B_Content.js
+++ src/components/BodyBComp/B_Content.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 function B_Content() {
   return (
-    <div>B_Content
+    <div className='b-container'>
+
+      <div className='bb-container'>
+
+        <div className='bb-title-box'>
+          <h2 className='bb-title'><i>Projects && EXP X))</i></h2>
+        </div>
+
+        <div className='bb-content-box'>
+          
+        </div>
+
+      </div>
+
     </div>
   )
 }
